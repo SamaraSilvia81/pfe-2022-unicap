203ca632c4928044323916ebc761b1162285850b
Samara Silvia <100232025+SamaraSilvia81@users.noreply.github.com>
2022-10-28 08:22:43 -0300

    Add files via upload

---
+++ my-app-arlequina/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Arlequina from "./components/Arlequina";
+
+function App() {
+
+  const titulo = "Harley Quinn"
+
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <div>
+        <Arlequina/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app-arlequina/src/components/Arlequina.js
@@ -0,0 +1,24 @@
+function Arlequina() {
+  
+    return (
+
+        <div className="section2">
+            <img src = "https://1.bp.blogspot.com/-jUar2XLp1LY/XkA0MHvnCkI/AAAAAAABB4o/Y-8iANwlFasYFT3XKU5JmpS66aD9_taUgCNcBGAsYHQ/d/aves-de-rapina-arlequina.jpg" className="photoBase"></img>
+            
+            <div>
+                <h2 className="subtitulo">Quem é a Arlequina?</h2>
+                <p className="corpoDoTexto"> <span className="destaque">Arlequina</span> possui uma personalidade muito submissa, sempre se entregando ao 
+                Coringa e fazendo suas vontades. Além disso, Arlequina já foi psiquiatra, então apresenta uma grande inteligência, 
+                embora nem sempre percebamos analisando de forma ''crua''. Após se unir ao Coringa, adquiriu um comportamento muito 
+                semelhante ao de seu amado, se tornando uma <span className="destaque">personagem insana e maluca</span>, fazendo de tudo por diversão. Em algumas HQs 
+                mais modernas, ela possui um comportamento menos desequilibrado, tendo em raras ocasiões uma certa seriedade.</p>
+            </div>
+
+            <a href="https://worldsvillians.netlify.app/arlequina.html"><button className="botao" onClick>Are you crazy?</button></a>
+        
+        </div>
+
+    );
+  }
+
+export default Arlequina;
